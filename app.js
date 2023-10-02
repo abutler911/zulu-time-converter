26ec95f871e8df120529d62e4cd41dc33b8df7a6
Andrew Butler <abutler911@gmail.com>
2023-10-02 10:52:50 -0600

    added css

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const moment = require("moment");
 const momentTimezone = require("moment-timezone");
-const port = 3000;
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const localTime = moment().format("YYYY-MM-DD HH:mm:ss");
+  const localTime = moment().format("MM-DD-YYYY HH:mm:ss");
   const zuluTime = moment.utc().format("YYYY-MM-DD HH:mm:ss");
+  const dayOfWeek = moment().format("dddd");
+  const localTimeZone = moment.tz.guess();
 
-  res.render("index", { localTime, zuluTime });
+  res.render("index", { localTime, zuluTime, dayOfWeek, localTimeZone });
 });
 
 app.listen(3000, () => {
-  console.log(`Server up on port ${port}...`);
+  console.log("Server running on http://localhost:3000");
 });
